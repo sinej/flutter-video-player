cffe7315c14b411085c5a11b54393528d95a6cec
shin <sinej1020@gmail.com>
2024-05-19 23:34:52 +0900

    first commit

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'asset/image/logo.png',
+          ),
+        ],
+      )
+    );
+  }
+}
